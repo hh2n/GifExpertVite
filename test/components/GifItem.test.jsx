4885f0753af02d2f3358ea8d920a2ea5f08f1d68
Cesar Espinosa <arionespinosam@gmail.com>
2023-01-03 14:34:23 -0600

    Agragando archivos de configuracion para Jest testing

---
+++ test/components/GifItem.test.jsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react"
+import { GifItem } from "../../src/components/GifItem"
+
+describe('Pruebas en <GifItem />', () => {
+
+    const title = 'Saitama'
+    const url = 'https://one-punch.com/saitama.jpg'
+    
+    test('debe de hacer match con el snapshot', () => {
+
+        const { container } = render( <GifItem title={title} url={url} />)
+        expect( container ).toMatchSnapshot()
+
+     })
+
+ })
