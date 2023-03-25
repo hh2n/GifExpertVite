5fd8a744b340b8a0661e5b369753d94d95f9593f
cesar arion espinosa mendez <arionespinosam@gmail.com>
2023-03-25 13:55:18 -0600

    Implementando pruebas

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,6 @@
 export const getGifs = async (category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=pwqflfumd1BzeT0R7YGu8gASCUz3LUGQ&q=${category}&limit=20`
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=pwqflfumd1BzeT0R7YGu8gASCUz3LUGQ&q=${category}&limit=30`
     const resp = await fetch(url)
     const { data } = await resp.json()
 
--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { GifItem } from "../../src/components/GifItem"
 
 describe('Pruebas en <GifItem />', () => {
@@ -11,6 +11,23 @@ describe('Pruebas en <GifItem />', () => {
         const { container } = render( <GifItem title={title} url={url} />)
         expect( container ).toMatchSnapshot()
 
+     })
+
+     test('debe de mostar la imagen con el URL y el ALT indicado', () => {
+
+        render(<GifItem title={ title } url={ url } />)
+        //screen.debug()
+        //expect( screen.getByRole('img').src ).toBe( url )
+        //expect( screen.getByRole('img').alt ).toBe( title )
+        const { src, alt } = screen.getByRole('img')
+        expect( src ).toBe( url )
+        expect( alt ).toBe( alt )
+
+     })
+
+     test('debe de mostrar el titulo en el componente', ()=>{
+        render(<GifItem title={ title } url={ url } />)
+        expect( screen.getByText( title ) ).toBeTruthy()
      })
 
  })
